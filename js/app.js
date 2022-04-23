2e908a7186b7c23cc19f4c23b8675fbece62cf1d
ANSONJOEL11 <ansonjoel11@gmail.com>
2022-04-22 21:38:26 -0400

    notif

--- js/app.js
+++ js/app.js
@@ -10,3 +10,66 @@ if ( 'serviceWorker' in navigator ) {
     console.log( 'ServiceWorker registration failed. Error: ' + err);
   });
 }
+
+const elements = {
+  notifyActionsBtn: document.querySelector('#notify-actions'),
+}
+
+const serviceWorkerNotify = async (title, msg) => {
+  const registration = await navigator.serviceWorker.ready;
+  if (registration) return registration.showNotification(title, msg);
+}
+
+const notify = (title, msg) => !msg?.actions ? new Notification(title, msg) : serviceWorkerNotify(title, msg);
+
+const askPermission = async () => {
+  // Is Web Notifications available on the browser
+  if(!("Notification" in window)) {
+    console.error("Notification API is not available on this device!");
+    return false;
+  }
+
+  // Did the user previously allow notifications
+  if (Notification.permission === 'granted') {
+    return true;
+  }
+
+  // If the user denied or hasn't been asked yet
+  if (Notification.permission === 'denied' || Notification.permission === 'default') {
+    try {
+      // Ask for permission
+      const permission = await Notification.requestPermission();
+      if (permission === 'granted') {
+        return true;
+      }
+      return false;
+    } catch (e) {
+      console.error("There was an issue acquiring Notification permissions", e);
+      return false;
+    }
+  }
+  return false;
+}
+
+const notifyActions = async () => {
+  const permission = await askPermission();
+  if (permission) {
+    const title = "Transfer Request!"
+    const msg = {
+      badge: "badge.png",
+      tag: 'transfer-request',
+      icon: 'badge.png',
+      image: 'transfer.png',
+      body: "You have an incoming transfer of 10000000 vbux!",
+      actions: [
+        { action: "accept", title: "Accept", icon: "yes.png" },
+        { action: "decline", title: "Decline", icon: "no.png" },
+      ]
+    }
+    const rslt = notify(title, msg);
+    console.log('Success!', rslt);
+  }
+}
+
+
+elements.notifyActionsBtn.addEventListener('click', notifyActions);
